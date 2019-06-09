e55fa962379f1e5c94bc8217896dabf016ded2d9
Luk Janine <janine.luk@avast.com>
2019-06-09 17:40:06 +0100

    creating json objects

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,19 +2,29 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello Express')
+    res.send('<h1>Hello Express</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send([{
+        name: 'Janine',
+        age: 23
+    }, {
+        name: 'Sarah',
+        age: 27
+    }])
 })
 
 app.get('/about', (req,res) => {
-    res.send('About Page')
+    res.send('<h1>This is the about page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('View Weather')
+    res.send([{
+        forecast: 'this is the current forecast'
+    }, {
+        location: 'this is the current location'
+    }])
 })
 
 app.listen(3000, () => {
