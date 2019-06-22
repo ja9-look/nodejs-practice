8e2765ae4e58acb4f4d6f1272666646a6056a774
Luk Janine <janine.luk@avast.com>
2019-06-22 09:55:52 +0100

    intro to handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,29 @@ const express = require('express')
 const app = express()
 const publicDir = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDir))
+
+
+app.get('', (req, res) => {
+    res.render('index',{
+    title: 'Weather App',
+    name: 'Janine L'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page'
+    })
+})
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    message: "Welcome to the Help Page. How may we help you today?"
+  });
+});
 
 app.get('/weather', (req, res) => {
     res.send([{
